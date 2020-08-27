aa16f99b0818db1e06aa4f39056ac51baa51d6ed
paulchabz <paulwchabz@gmail.com>
2020-08-27 22:14:56 +0100

    Logic

--- MiniProject/Program.cs
+++ MiniProject/Program.cs
@@ -10,6 +10,48 @@ namespace MiniProject
     {
         static void Main(string[] args)
         {
+            // Console Application 
+            // Ask the user for the first name 
+            Console.Write("What is your first name? ");
+            string firstName = Console.ReadLine();
+
+            // Ask the user for their age
+            Console.Write("What is your age? ");
+            string ageText = Console.ReadLine();
+
+            // Convert the ageText to age
+            bool isValidAge = int.TryParse(ageText, out int age);
+            // if age is not valid 
+            if (isValidAge == false)
+            {
+                Console.WriteLine("That is not a valid age please input a number.");
+                Console.ReadLine(); // pauses the app 
+                return; // closes out the main method {application}
+            }
+
+            // check for first name 
+            string formattedName = "";
+            if (firstName.ToLower() == "bob" || firstName.ToLower() == "sue")
+            {
+                // name = professor
+                formattedName = $"Professor {firstName}";
+            }
+            else
+            {
+                formattedName = firstName;
+            }
+
+            // check for age less than 21 
+            if (age < 21)
+            {
+                Console.WriteLine($"I recommend you wait {21 - age} years before you start this class {formattedName} ");
+            }
+            else
+            {
+                Console.WriteLine($"Welcome to the class {formattedName} :) ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
